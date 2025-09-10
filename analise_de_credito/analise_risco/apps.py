9ba2d72c008f5bcf45ff55268f4f34f286c747c1
pedro <pedrobento131@gmail.com>
2025-09-10 20:00:52 -0300

    config ambiente

--- analise_de_credito/analise_risco/apps.py
+++ analise_de_credito/analise_risco/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class RiscoDeCreditoConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'risco_de_credito'
+    name = 'analise_risco'
